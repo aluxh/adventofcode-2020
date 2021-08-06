f7e424a455c2c736ff02751e54ec9ed62517522d
aluxh <alex.hochunsiong@gmail.com>
2021-08-06 11:03:50 +0800

    finished day7 part 2

--- day7.R
+++ day7.R
@@ -1,18 +1,18 @@
 # Advent Of Code 2020 - Day 7 ----
 # Day 7: Handy Haversacks
+library(stringr)
+library(magrittr)
+
 # PART 1 ----
-
 input <- readLines("input/day7.txt")
-
-cleaned_input <- gsub(pattern = " bags", replacement = "", x = input)
+cleaned_input <- gsub(pattern = " (bags|bag)", replacement = "", x = input)
+cleaned_input <- gsub(pattern = ".$", replacement = "", x = cleaned_input)
 cleaned_input <- strsplit(cleaned_input, split = " contain ")
 
-
-# Search
+# Search Bags
 track_bags <- function(results = list(), bag_list, search_color) {
     
     index <- grep(lapply(bag_list, "[[", 2), pattern = search_color)
-    
     if (length(index) == 0) return (results)
     
     found_bag <- bag_list[index]
@@ -21,73 +21,43 @@ track_bags <- function(results = list(), bag_list, search_color) {
     return (track_bags(c(results, next_search), bag_list, next_search))
 }
 
-first <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = "shiny gold")]
-first_layer <- paste(lapply(first, "[[", 1), collapse = "|")
+result <- unlist(track_bags(bag_list = cleaned_input, search_color = "shiny gold"))
+color_bags <- unique(unlist(strsplit(result, split = "|", fixed = TRUE)))
+
+# Number of color bags contain at least one shiny gold bag
+length(color_bags)
 
 
-second <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = first_layer)]
-second_layer <- paste(lapply(second, "[[", 1), collapse = "|")
+# PART 2 ----
+cleaned_input
 
+# 1. Look into the list of bags and find the parent bag.
+# 2. Retrieve how many children bags are in the parent bag. Sum the count and multiply by the number of parent bags.
+# 3. Retrieve the colors of the children bags inside the parent bags. Then, for each color, return to step 1.
 
-third <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = second_layer)]
-third_layer <- paste(lapply(third, "[[", 1), collapse = "|")
+# Retrieve the bag names 
+bags <- sapply(cleaned_input, "[[", 1)
 
+# Assign the parent bags' names to each list
+list_of_bags <- setNames(strsplit(sapply(cleaned_input, "[[", 2), split = ", ", fixed = TRUE), bags)
 
-fourth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = third_layer)]
-fourth_layer <- paste(lapply(fourth, "[[", 1), collapse = "|")
+count_bag <- function(bag_list, search_color) {
+    
+    bag_count <- sapply(bag_list[[search_color]], function(x) {
+        str_extract(x, "\\d+(?=\\s)") %>% 
+            as.numeric()
+    })
+    
+    next_search <- sapply(bag_list[[search_color]], function(x) {
+        str_extract(x, "(?!\\d+\\s)(\\w+\\s\\w+)") 
+    })
+    
+    sum(map2_dbl(bag_count, next_search, function(x, y) {
+        x * (1 + count_bag(bag_list, y))
+    }), na.rm = TRUE)
+}
 
-
-fifth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = fourth_layer)]
-fifth_layer <- paste(lapply(fifth, "[[", 1), collapse = "|")
-
-
-sixth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = fifth_layer)]
-sixth_layer <- paste(lapply(sixth, "[[", 1), collapse = "|")
-
-
-seventh <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = sixth_layer)]
-seventh_layer <- paste(lapply(seventh, "[[", 1), collapse = "|")
-
-
-eighth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = seventh_layer)]
-eighth_layer <- paste(lapply(eighth, "[[", 1), collapse = "|")
-
-
-ninth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = eighth_layer)]
-ninth_layer <- paste(lapply(ninth, "[[", 1), collapse = "|")
-
-
-tenth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = ninth_layer)]
-tenth_layer <- paste(lapply(tenth, "[[", 1), collapse = "|")
-
-
-eleventh <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = tenth_layer)]
-eleventh_layer <- paste(lapply(eleventh, "[[", 1), collapse = "|")
-
-
-twelveth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = eleventh_layer)]
-twelveth_layer <- paste(lapply(twelveth, "[[", 1), collapse = "|")
-
-
-thirteenth <- cleaned_input[grep(lapply(cleaned_input, "[[", 2), pattern = twelveth_layer)]
-
-
-
-
-bags <- c(first_layer, second_layer, third_layer, fourth_layer, fifth_layer, sixth_layer, seventh_layer, eighth_layer, ninth_layer,
-          tenth_layer, eleventh_layer, twelveth_layer)
-bags
-result <- unique(unlist(strsplit(first, split = "|", fixed = T)))
-length(result)
-
-
-
-
-
-
-
-
-
+count_bag(list_of_bags, "shiny gold")
 
 
 
---
+++ others_ans_day7.R
@@ -0,0 +1,31 @@
+library(stringr)
+library(purrr)
+
+rules <- readLines("input/day7.txt")
+
+# Tidying
+# Get bag part
+tidy_rules <- str_split_fixed(rules, " contain ", 2)
+bags <- gsub("\\s*bags\\s*", "", tidy_rules[,1])
+
+bags_list <- str_match_all(tidy_rules[,2], "(\\d+) (\\w+ \\w+)")
+bags_list <- setNames(lapply(bags_list, function(x) setNames(x[,2], nm = x[,3])) , 
+                      bags)
+
+ans1 <- function(bag_col){
+    bags <- names(bags_list)[map_lgl(bags_list, ~bag_col %in% names(.x))]
+    c(bags, unlist(lapply(bags, ans1)))
+}
+
+# Answer 1
+length(unique(ans1("shiny gold")))
+
+# Answer 2
+ans2 <- function(bag_col){
+    inside <- bags_list[[bag_col]]
+    sum(map2_dbl(as.numeric(inside), names(inside), 
+                 function(x,y) x * (1 + ans2(y)) ))
+    # count bag and the items inside
+}
+
+ans2('shiny gold') 
